ab4d45e8672b23f4aa299d57662fe2c8b52cd112
Femi Dokun <30286598+Femi1992@users.noreply.github.com>
2018-07-31 14:13:39 +0100

    Add files via upload

---
+++ CSProject/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace CSProject
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<Staff> myStaff = new List<Staff>();
+            FileReader fr = new FileReader();
+            int year = 0;
+            int month = 0;
+
+            while (year == 0)
+            {
+                Console.Write("\nPlease enter the year: ");
+
+                try
+                {
+                    year = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message + " Please try again.");
+                }
+            }
+
+            while (month == 0)
+            {
+                Console.WriteLine("\nPlease enter month: ");
+
+                try
+                {
+                    month = Convert.ToInt32(Console.ReadLine());
+                    if (month < 1 || month > 12)
+                    {
+                        Console.WriteLine("Invalid value");
+                        month = 0;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message + ". Please try again");
+                }
+            }
+
+            myStaff = fr.ReadFile();
+
+            for (int i = 0; i < myStaff.Count; i++)
+            {
+                try
+                {
+                    Console.Write("Please enter number of hours worked for {0} ", myStaff[i].NameOfStaff);
+                    myStaff[i].HoursWorked = Convert.ToInt32(Console.ReadLine());
+                    myStaff[i].CalculatePay();
+                    Console.WriteLine(myStaff[i].ToString());
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    i--;
+                }
+            }
+
+            PaySlip ps = new PaySlip(month, year);
+            ps.GeneratePaySlip(myStaff);
+            ps.GeneratePaySlip(myStaff);
+            Console.Read();
+
+        }
+    }
+}
---
+++ CSProject/Staff.cs
@@ -0,0 +1,114 @@
+﻿using System;
+namespace CSProject
+{
+    class Staff
+    {
+        private float hourlyRate;
+        private int hWorked;
+        private string name;
+        private float rate;
+
+        public float TotalPay { get; protected set; }
+        public float BasicPay { get; private set; }
+        public string NameOfStaff { get; private set; }
+        public int HoursWorked
+        {
+            get
+            {
+                return hWorked;
+            }
+
+            set
+            {
+                if (value > 0)
+                    hWorked = value;
+                else
+                    hWorked = 0;
+            }
+        }
+
+
+        public Staff(string name, float rate)
+        {
+            NameOfStaff = name;
+            hourlyRate = rate;
+
+        }
+
+        public virtual void CalculatePay()
+        {
+            Console.WriteLine("Calculating Pay...");
+            BasicPay = hWorked * hourlyRate;
+            TotalPay = BasicPay;
+        }
+
+        public override string ToString()
+        {
+            return "\nNameOfStaff = " + NameOfStaff
+                + "\nhourlyRate = " + hourlyRate + "\nhWorked = " + hWorked
+                + "\nBasicPay = " + BasicPay + "\n\nTotalPay = " + TotalPay;
+        }
+    }
+
+
+    class Manager : Staff
+    {
+        private const float managerHourlyRate = 50;
+        public int Allowance {get; private set;}
+
+
+        public Manager(string name) : base(name, managerHourlyRate){}
+       
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+            Allowance = 0;
+            if (HoursWorked >= 160)
+            {
+                Allowance = 1000;
+                TotalPay += Allowance;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "\nNameOfStaff = " + NameOfStaff + "\nmanagerHourlyRate = "
+                + managerHourlyRate + "\nHoursWorked = " + HoursWorked + "\nBasicPay = "
+                + BasicPay + "\nAllowance = " + Allowance + "\n\nTotalPay = " + TotalPay;
+        }
+    }
+
+
+    class Admin : Staff
+    {
+        private const float overTimeRate = 15.5f;
+        private const float adminHourlyRate = 30;
+
+        public float Overtime {get; private set;}
+
+
+        public Admin(string name) : base(name, adminHourlyRate) {}
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+
+            if (HoursWorked > 160)
+            {
+                Overtime = overTimeRate * (HoursWorked - 160);
+                TotalPay = BasicPay + Overtime;
+            }
+                
+        }
+
+        public override string ToString()
+        {
+            return "\nNameOfStaff = " + NameOfStaff + "\adminHourlyRate = "
+                + adminHourlyRate + "\nHoursWorkd = " + HoursWorked + "\nBasicPay = "
+                + BasicPay + "\nOvertime = " + Overtime + "\n\nTotalPay = " + TotalPay;
+        }
+
+    }
+
+
+}
